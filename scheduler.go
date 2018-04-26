575702d6ec9a8ca8baf498c7295e63a96b6061f5
marcmolla <marcmolla@gmail.com>
2018-04-26 16:00:39 +0200

    rollback

--- scheduler.go
+++ scheduler.go
@@ -301,8 +301,7 @@ func (sch *scheduler) selectPathDeepLearning(s *session, hasRetransmission bool,
 	}
 
 	pathID, err := sch.agent.SelectPath(pathStats)
-	pathIDint := rand.Intn(len(pathStats))
-	pathID = pathStats[pathIDint].pathID
+	
 	if err != nil {
 		panic(err)
 	}
